48fba36dcad17142b94074d29d3df7ede263884a
AnnaTran96 <anna_tran@hotmail.co.uk>
2021-10-30 09:50:43 +0100

    hide api key

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express()
 const request = require('request')
 const cors = require('cors')
 
+const API_KEY = process.env.REACT_APP_API_KEY
+
 const corsOptions = {
     origin: 'https://api.getAddress.io',
 }
-
 
 app.get('/cors', (req, res) => {
     res.send('This has CORS enabled')
@@ -15,7 +16,7 @@ app.get('/cors', (req, res) => {
 app.get('/', cors(corsOptions), (req, res) => {
     res.set('Access-Control-Allow-Origin', '*')
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    const url = 'https://api.getAddress.io/autocomplete/london?api-key=odHqOd1MsECNFi_4xTyXtw33193'
+    const url = `https://api.getAddress.io/autocomplete/london?api-key=${API_KEY}`
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body);
@@ -24,18 +25,10 @@ app.get('/', cors(corsOptions), (req, res) => {
     })
 })
 
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
-
 app.post('/', cors(corsOptions), (req, res) => {
     res.set('Access-Control-Allow-Origin', '*')
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    const url = 'https://api.getAddress.io/private-address/se2wsq?api-key=W7Ky26qT5EeUMsi0t1R0LA33168'
+    const url = `https://api.getAddress.io/private-address/se2wsq?api-key=${API_KEY}`
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body);
